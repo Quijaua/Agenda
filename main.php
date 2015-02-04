788e52eee13f6fc3c27db0eb3d69fb2a7f1c223b
Eduardo Alencar <ealencar10@gmail.com>
2015-02-04 00:17:35 -0200

    Arquivo principal do Plugin

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+/**
+ * Plugin Name: Quijaua - Eventos
+ * Description: Gerenciamento de eventos
+ * Version: 0.0.1
+ * Author: Eduardo Alencar de Oliveira
+ * License: GPL3
+ */
+
